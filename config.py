cf9466360e90684b5da97cecc1aa590211358798
mpertierra <marcosp@mit.edu>
2016-05-14 22:13:37 -0400

    Production config should actually use a secret key.

--- config.py
+++ config.py
@@ -5,12 +5,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
 
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    try:
+        SECRET_KEY = os.environ['SECRET_KEY']
+    except KeyError:
+        SECRET_KEY = os.urandom(64)
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "postgres:////tmp/test.db"
+    SECRET_KEY = 'development-key'
