322f5a02fc3031bc9b228757e4cfdbd2302ca0f2
mpertierra <marcosp@mit.edu>
2016-05-14 20:37:39 -0400

    Bug fix: Invalid query in get_chats_for_user().

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test.db"
+    SQLALCHEMY_DATABASE_URI = "postgres:////tmp/test.db"
