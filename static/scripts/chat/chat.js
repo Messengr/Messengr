3243364742c43247d959b5c78f60f7e8db84ae67
Anthony <tonypr100@gmail.com>
2016-05-18 02:25:13 -0400

    Search click fix

--- static/scripts/chat/chat.js
+++ static/scripts/chat/chat.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     $("#chat_base").scrollTop($("#chat_base")[0].scrollHeight);
     
     // Handle Search
-    $('#send_message').click(function() {
+    $('#send_keyword').click(function() {
         var keyword = $('#search_text').val().toLowerCase();
         
         if (!symmetric_key) {
