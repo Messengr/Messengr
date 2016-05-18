ce5b3b43bd1a21b8ba7b4ba27ce8bc202cc14b3f
mpertierra <marcosp@mit.edu>
2016-05-18 12:44:38 -0400

    Remove String.format definition; no longer used.

--- static/scripts/chat/chat.js
+++ static/scripts/chat/chat.js
@@ -1,16 +1,3 @@
-// Useful format function
-if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number] 
-        : match
-      ;
-    });
-  };
-}
-
 $(document).ready(function(){
     var socket;
     var symmetric_key;
