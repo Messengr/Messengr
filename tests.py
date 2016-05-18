189fb5d33b85f1b6372f7b9ccf0d3749ccdec612
Anthony <tonypr100@gmail.com>
2016-05-18 18:22:41 -0400

    Add tests file

---
+++ tests.py
@@ -0,0 +1,19 @@
+import os
+import app
+import unittest
+import tempfile
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+if __name__ == '__main__':
+    unittest.main()
