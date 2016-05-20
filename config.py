2d3d362517c381bd87bd4f5898536731541ed827
Anthony <tonypr100@gmail.com>
2016-05-20 03:34:24 -0400

    Added testing config portion and login/logout tests

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -18,3 +19,11 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test.db"
     SECRET_KEY = 'development-key'
+
+class TestingConfig(Config):
+    DEBUG = True
+    temp_file = tempfile.mkstemp()
+    DB_FD = temp_file[0]
+    DATABASE_FILE = temp_file[1]
+    SQLALCHEMY_DATABASE_URL = "sqlite:////" + temp_file[1]
+    SECRET_KEY = 'testing-key'
--- tests.py
+++ tests.py
@@ -1,24 +1,36 @@
 import os
 
-os.environ["DATABASE_URL"] = "testdb"
-os.environ["APP_SETTINGS"] = "config.DevelopmentConfig"
+os.environ["DATABASE_URL"] = "sqlite:////tmp/test.db"
+os.environ["APP_SETTINGS"] = "config.TestingConfig"
 
 import app as SecureMessenger
 import unittest
-import tempfile
 
 import models
+
+user_alice = {
+    'username' : 'alice',
+    'password' : 'testpass1234',
+    'public_key' : '3zaXNLdZvashITmql64jBotAiusoX4iaFnHv4ubxh+nV5UhtvMac1rKRmM5loAeIlDB7n9ZlMNJPl298/Hd43A==',
+    'secret_key' : 'IDhMGp6hRzWH6xh/+uIn7RVD1ye71ZvkuJPSK8IcU74='
+}
+
+user_bob = {
+    'username' : 'bob',
+    'password' : 'testpass1234',
+    'public_key' : '0XOaiEAOzkB7eZfIkaAKlrhh0vE6IMV9G8XtBn72ZIjGEDfxiJlHm0CRbonjYKrHEL6jUT3WVmqpdvWNSvrsKg==',
+    'secret_key' : '09RGnoJcKJH3+ILWzv/MKatoOdLQZnJNPRDafSCOLlM='
+}
 
 class BaseTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, SecureMessenger.app.config['DATABASE'] = tempfile.mkstemp()
         SecureMessenger.app.config['TESTING'] = True
         self.app = SecureMessenger.app.test_client()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(SecureMessenger.app.config['DATABASE'])
+        os.close(SecureMessenger.app.config['DB_FD'])
+        os.unlink(SecureMessenger.app.config['DATABASE_FILE'])
         
     def create_account(self, username, password, public_key):
         return self.app.post('/user/create', data=dict(
@@ -37,11 +49,31 @@ class BaseTestCase(unittest.TestCase):
         return self.app.get('/logout', follow_redirects=True)
 
 class LoginTestCase(BaseTestCase):
-    def test_login_unauthenticated(self):
+    def test_login_logout(self):
+        ## Test Unauthenticated
         rv = self.login('test', 'test')
         self.assertIn("Error: Invalid username and/or password", rv.data)
         self.assertEqual(rv.status_code, 200)
-
         
+        ## Test create account and authenticated login
+        rv = self.create_account(user_alice["username"], user_alice["password"], user_alice["public_key"])
+        self.assertIn(user_alice["username"], rv.data)
+        self.assertEqual(rv.status_code, 200)
+        
+        rv = self.login(user_alice["username"], user_alice["password"])
+        self.assertIn(user_alice["username"], rv.data)
+        self.assertEqual(rv.status_code, 200)
+        
+        rv = self.logout()
+        self.assertIn("Login", rv.data)
+        self.assertEqual(rv.status_code, 200)
+        
+#class ChatTestCase(BaseTestCase):
+#    def test_chat(self):
+#        ## Test create account and authenticated login
+#        rv = self.create_account(user_alice["username"], user_alice["password"], user_alice["public_key"])
+#        self.assertIn(user_alice["username"], rv.data)
+#        self.assertEqual(rv.status_code, 200)
+    
 if __name__ == '__main__':
     unittest.main()
